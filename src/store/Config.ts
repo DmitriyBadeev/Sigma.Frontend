83873e96e938469a404fdeeb580d4682d7ff97d4
DmitriyBadeev <dmitriibadeev@gmail.com>
2021-10-16 16:07:25 +0500

    убираем https

--- src/store/Config.ts
+++ src/store/Config.ts
@@ -1,7 +1,7 @@
 import { UserManagerSettings, WebStorageStateStore } from "oidc-client"
 
 export const prodConfig: UserManagerSettings = {
-    authority: "https://identity-sigma.herokuapp.com",
+    authority: "http://identity-sigma.herokuapp.com",
     client_id: "Sigma.Spa",
     redirect_uri: "https://sigma-frontend.herokuapp.com/auth-complete",
     response_type: "code",
@@ -13,21 +13,8 @@ export const prodConfig: UserManagerSettings = {
     silent_redirect_uri: "https://sigma-frontend.herokuapp.com/silent.html",
 }
 
-// export const devConfig: UserManagerSettings = {
-//     authority: "https://auth.badeev.info",
-//     client_id: "finance_investin",
-//     redirect_uri: "http://localhost:3000/auth-complete",
-//     response_type: "code",
-//     loadUserInfo: true,
-//     scope: "openid profile InvestIn.Finance.API",
-//     post_logout_redirect_uri: "http://localhost:3000/signout",
-//     userStore: new WebStorageStateStore({ store: window.localStorage }),
-//     automaticSilentRenew: true,
-//     silent_redirect_uri: "http://localhost:3000/silent.html",
-// }
-
 export const devConfig: UserManagerSettings = {
-    authority: "https://identity-sigma.herokuapp.com",
+    authority: "http://identity-sigma.herokuapp.com",
     client_id: "Sigma.Spa",
     redirect_uri: "http://localhost:3000/auth-complete",
     response_type: "code",
